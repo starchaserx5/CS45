4fb33d1e3977e6c4f3cab3928ab4139795a54abc
Huy Nguyen <huynguyen2412@gmail.com>
2018-11-15 23:47:43 -0800

    fixed bitset LCD

--- lab5/LCD.cpp
+++ lab5/LCD.cpp
@@ -36,13 +36,13 @@ bool getInput(string& line)
 
 void loadLCD(map<int,int>& lcdMap)
 {
-  lcdMap[0] = 31;    //{0,1,2,3,4,5}
-  lcdMap[1] = 3;     //{1,2}
-  lcdMap[2] = 109;    //{0,1,3,4,6}
-  lcdMap[3] = 121;    //{0,1,2,3,6}
-  lcdMap[4] = 51;     //{1,2,5,6}
-  lcdMap[5] = 91;     //{0,2,3,5,6}
-  lcdMap[6] = 95;     //{0,2,3,4,5,6} 
+  lcdMap[0] = 63;    //{0,1,2,3,4,5}
+  lcdMap[1] = 6;     //{1,2}
+  lcdMap[2] = 91;    //{0,1,3,4,6}
+  lcdMap[3] = 79;    //{0,1,2,3,6}
+  lcdMap[4] = 78;     //{1,2,5,6}
+  lcdMap[5] = 109;     //{0,2,3,5,6}
+  lcdMap[6] = 125;     //{0,2,3,4,5,6} 
   lcdMap[7] = 7;      //{0,1,2}
   lcdMap[8] = 127;    //{0,1,2,3,4,5,6}
   lcdMap[9] = 123;    //{0,1,2,3,5,6}  
@@ -55,15 +55,13 @@ void printLCD(int input)
   cout<<"The number segment : {";
   for(int i=0;i<bitNum.size();i++)
   {
-    if(index == 0)
+    if (index == 0 && bitNum.test(i))
     {
-      if(bitNum.test(i))
-        cout <<i;
+      cout <<i;
       index++;
     }
-    else
+    else if (index != 0 && bitNum.test(i))
     {
-      if(bitNum.test(i))
         cout << "," << i;
     }
   }
